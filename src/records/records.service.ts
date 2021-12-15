4f0c310123334465dfeb0220e3320a881499f13a
sariforest <iread1q84@gmail.com>
2021-12-15 12:56:07 +0900

    Try to use interface

--- src/records/records.service.ts
+++ src/records/records.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+const testArray = ["test1", "test2"];
+
+class plans { // interface(or 같은 역할을 하는 class)의 이름은 어떻게 정함 ?
+    public showAllRecords(): string[];
+    /* public addRecords(): void;
+    public updateRecords(): void;
+    public deleteRecords(): void;
+    public searchForRecords(): string[];
+    public showRelatedNews(): string[];
+    public showMixedRecords(): string[]; */
+};
+
 @Injectable()
-export class RecordsService {
+export class RecordsService extends plans {
     getHello(): string {
         return 'Hello World from records module!';
     }
+
+    showAllRecords(): string[] {
+        return testArray;
+    };
+
 }
